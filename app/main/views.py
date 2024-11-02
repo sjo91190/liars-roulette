ca93d28e8eddf4395d9365f126837e6f87bd73bf
Sam OKeefe <sjo91190@gmail.com>
2024-11-02 03:25:47 -0600

    docstrings and cleaning up views. Small changes to config and roulette

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,6 @@
+"""
+Views and routes for roulette
+"""
 from flask import request, render_template, redirect, url_for
 from app.main import main
 from app.main.forms import PlayerNameForm, PlayerCountForm
@@ -8,19 +11,26 @@ config = dict()
 
 @main.route("/", methods=['GET', 'POST'])
 def index():
+    """
+    Index page where user selects the amount of players
+    Form used limits to 6 players
+    :return: Returns index template if GET, assign template if POST
+    """
     form = PlayerCountForm()
 
     if request.method == "GET":
         return render_template("index.html", form=form)
 
     config['count'] = int(form.number.data)
-    print(config['count'])
-
     return redirect(url_for("main.assign"))
 
 
 @main.route("/assign", methods=['GET', 'POST'])
 def assign():
+    """
+    Player names created and Player objects created
+    :return: assign template of GET, play if POST
+    """
     config['names'] = []
     number = config["count"]
     players = {f"player{i}": f"Player {i + 1}" for i in range(number)}
@@ -36,18 +46,18 @@ def assign():
             config['names'].append(form.data.get(f"player{i}"))
 
         config['liars'] = create(player_list=config['names'])
-        print(config['liars'])
-
         return redirect(url_for("main.play"))
 
 
 @main.route("/liar", methods=['GET', 'POST'])
 def play():
+    """
+    Main game loop. Player selected undergoes a roll and stats are updated
+    :return: returns play for GET and POST
+    """
 
     if request.method == "GET":
         return render_template("play.html", liars=config.get('liars'))
 
-    print(request.form['person'])
     config['liars'][request.form['person']].roll()
-
     return redirect(url_for("main.play"))
--- roulette.py
+++ roulette.py
@@ -5,8 +5,8 @@ from app import app_factory
 
 if os.environ.get("LIARS_CONFIG") == "prod":
     if __name__ == "__main__":
-        host = os.environ.get("HOST")
-        port = os.environ.get("PORT")
+        host = os.environ.get("HOST") or "0.0.0.0"
+        port = os.environ.get("PORT") or 80
         app = app_factory(config_name="prod")
         serve(TransLogger(app), host=host, port=port)
 
